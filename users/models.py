ea63f3dd6a8bd4909f54aed293d8c5c64fb57a77
Igorttdp <igorttdp.dev@email.com>
2023-02-23 14:37:36 -0300

    feat: add unique constraint for email field

--- users/models.py
+++ users/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
