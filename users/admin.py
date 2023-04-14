1cfcfc9c79af8bb6e8e7283508ac07ce08ccba82
Igorttdp <igorttdp.dev@email.com>
2023-04-14 10:27:31 -0300

    feat: add Model User on Admin Site and create a Custom User Admin model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        ("Credentials", {"fields": ("username", "password", "is_active")}),
+        ("Personal Info", {"fields": ("email", "first_name", "last_name")}),
+        ("Permissions", {"fields": ("is_staff", "is_superuser")}),
+        ("Dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
