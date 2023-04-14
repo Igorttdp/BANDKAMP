9b25bd6ab3a9ecc00719e4e738c9376462a31f35
Igorttdp <igorttdp.dev@email.com>
2023-04-14 12:20:00 -0300

    fix: call create_user instead create method

--- users/serializers.py
+++ users/serializers.py
@@ -41,7 +41,7 @@ class UserSerializer(serializers.ModelSerializer):
         if (validated_data["is_superuser"]):
             return User.objects.create_superuser(**validated_data)
 
-        return User.objects.create(**validated_data)
+        return User.objects.create_user(**validated_data)
 
     def update(self, instance: User, validated_data: dict) -> User:
         for key, value in validated_data.items():
