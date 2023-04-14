373f2f567c7198064a12f8614e9d81d4b4b5ae4d
Igorttdp <igorttdp.dev@email.com>
2023-04-14 10:27:05 -0300

    feat: add Model Song on Admin Site and create a Custom Song Admin model

--- songs/admin.py
+++ songs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Song
 
-# Register your models here.
+
+class CustomSongAdmin(admin.ModelAdmin):
+    list_display = ["title", "duration", "album", "id"]
+
+
+admin.site.register(Song, CustomSongAdmin)
